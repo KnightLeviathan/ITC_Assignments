1a3434ac47c3a21928de389d390101059d997140
Knight-Fenris <ribhavaggarwal.bt22ece@pec.edu.in>
2022-12-24 16:49:11 +0530

    Update as3c.py

--- Assignment3/as3c.py
+++ Assignment3/as3c.py
@@ -1,7 +1,7 @@
-str_input = input("Enter numbers (using ',' or ' 'as separators) : ")
+str_input = input("Enter number(s) (using ',' or ' ' as separators) : ")
 if (' ' in str_input) : numbers = list(map(int, str_input.split()))
 elif(',' in str_input) :numbers = list(map(int, str_input.split(',')))
-elif(len(str_input) == 1): numbers = [int(str_input)]
+elif(' ' not in str_input and ',' not in str_input): numbers = [int(str_input)]
 else: raise ValueError("Please enter a valid input")
 
 #print(numbers)
